6a2f4c8ab26e0c466793a41afc2be1e85c6c08a0
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-03-07 13:58:49 -0500

    Commit Refactor Demo

--- JS_wCanvisAPI/index.js
+++ JS_wCanvisAPI/index.js
@@ -1,7 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
-  Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -18,13 +17,6 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 const walls = [
   Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
@@ -33,21 +25,6 @@ const walls = [
 ];
 World.add(world, walls);
 
-//Random Shapes
-
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35)
-    );
-  }
-}
 // const shape = Bodies.rectangle(200, 200, 50, 50, {
 //   isStatic: true,
 // });
