a9e72475977ec2ed0883b40c12481e96a24d88e4
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-04-06 12:42:10 -0400

    Commit index.js comments

--- List Comman/index.js
+++ List Comman/index.js
@@ -6,13 +6,17 @@ node mod- readdir
   require fs. for file system
 first argument pathopen 
   current directory '.'
+  *update: first param process.cwd()
+    cwd-current working directory
+  returns the cwd
 second argument 
   optional- options object
 third arg
   callback function with two args
     1st-an error object returns if(error) if no error returns null
+  ##common pattern in fs.'' dir callbacks that return an error object||null and the data requested if null
 */
-fs.readdir(".", (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
   //err === err obj, something wrong
   //err === null, everything okay
   if (err) {
